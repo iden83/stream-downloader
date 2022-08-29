13bfadf655e43202db82ef0ff05ca9bd7da4879c
Adam Krzysztofik <adam.krzysztofik@tii.ae>
2022-08-29 22:38:34 +0400

    Fix recursive delete dir issue and minor container name

--- download.sh
+++ download.sh
@@ -1,7 +1,8 @@
 mkdir -v -p `date +"%Y_%m_%d"`
 curl -L -m 3600 ${STREAM_LINK} -o `date +"%Y_%m_%d"`/${FILE_PREFIX}`date +"%Y_%m_%dT%H_%M"`.mp3
 
+echo
 echo "Files (and directories) subject to removal:"
 find . -name '*.mp3' -mtime +${FILE_REMOVAL_MTIME} -exec rm -v {} \;
-find . -type d -empty -name '????_??_??' -exec rmdir -v {} \;
+find . -maxdepth 1 -type d -empty -name '????_??_??' -exec rmdir -v {} \;
 
